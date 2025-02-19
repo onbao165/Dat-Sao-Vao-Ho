fef24d4d2af36193eab6ef3939869ca0746fd99c
barooon165 <112069172+onbao165@users.noreply.github.com>
2025-02-19 21:26:30 +0700

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-//   base: '/Partial-Permutation-Learning/'
+  base: '/Partial-Permutation-Learning/'
 })
